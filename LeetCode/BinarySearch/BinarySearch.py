aca896ddb73a88bafd57712e0b4d4e22143ff983
Aman Kumar <kumaraman73065@gmail.com>
2026-06-30 17:46:06 +0530

    binary search

---
+++ LeetCode/BinarySearch/BinarySearch.py
@@ -0,0 +1,17 @@
+# Quetion : Binary Search 
+# link : https://leetcode.com/problems/binary-search/description/
+
+def search(self, nums: List[int], target: int) -> int:
+        start , end = 0 , len(nums)-1
+        while start <= end:
+            mid =end - (end - start)//2
+            if nums[mid] == target:
+                return mid
+            if nums[mid] < target:
+                start = mid + 1 
+            else :
+                end = mid - 1 
+        
+        return -1 
+
+
---
+++ LeetCode/BinarySearch/firstnlast.py
@@ -0,0 +1,33 @@
+# Question : Find First and Last Position of Element in Sorted Array
+# link: https://leetcode.com/problems/find-first-and-last-position-of-element-in-sorted-array/description/
+
+def searchRange(self, nums: List[int], target: int) -> List[int]:
+        first = -1
+        last = -1 
+        start , end = 0 , len(nums) -1 
+        while start <= end:
+            mid = end - (end - start)//2
+            if nums[mid] == target:
+                first = mid 
+                end = mid - 1 
+            elif nums[mid] < target:
+                start = mid + 1 
+            else:
+                end = mid -1 
+        
+
+        start , end = 0 , len(nums) -1 
+        while start <= end:
+            mid = end - (end - start)//2
+            if nums[mid] == target:
+                last = mid 
+                start = mid  + 1 
+            elif nums[mid] < target:
+                start = mid + 1 
+            else:
+                end = mid -1 
+        return [first , last ]
+        
+            
+    
+        
